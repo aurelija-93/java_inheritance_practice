468436fea36a110b69f53e7dd7853be6a3ce593b
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-16 19:08:45 +0100

    added and tested Developer class

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+    Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Jim", "ABC789", 22222.33);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jim", developer.getName());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("ABC789", developer.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(22222.33, developer.getSalary(), 0.001);
+    }
 }
