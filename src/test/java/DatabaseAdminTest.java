df67c91a1522ae208fc06b3eae423cc1ce557a80
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-16 19:06:29 +0100

    added and tested DatabaseAdmin class

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
 public class DatabaseAdminTest {
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before() {
+        databaseAdmin = new DatabaseAdmin("Pam", "ABC456", 11111.22);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Pam", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNiNumber() {
+        assertEquals("ABC456", databaseAdmin.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(11111.22, databaseAdmin.getSalary(), 0.001);
+    }
 }
